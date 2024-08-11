e4d58fe9da281ca1d36ad770369d29e2781019db
eyad <eyadmuhammad90@gmail.com>
2024-08-12 01:04:31 +0300

    fixed bug

--- src/components/SidebarItem.tsx
+++ src/components/SidebarItem.tsx
@@ -1,6 +1,6 @@
-import { AnimatePresence,motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { SidebartItemType } from "../types/sidebarItemType";
-import {  NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const SidebarItem = ({
   to,
@@ -10,23 +10,28 @@ const SidebarItem = ({
 }: SidebartItemType & { isOpen: boolean }) => {
   return (
     <NavLink
+      end={true}
       to={to}
-      className={({isActive})=>`flex p-4 items-center font-medium rounded-lg gap-5 hover:bg-gray-700 ${isActive && 'bg-gray-700'}`}
+      className={({ isActive }) =>
+        `flex p-4 items-center font-medium rounded-lg gap-5 hover:bg-gray-700 ${
+          isActive && "bg-gray-700"
+        }`
+      }
     >
       <span className="flex-shrink-0">{icon}</span>
 
       <AnimatePresence>
-
-
-      {isOpen && <motion.span
-       initial={{opacity:0,width:0}}
-       animate={{opacity:1,width:'auto'}}
-       exit={{opacity:0,width:0}}
-
-       transition={{duration:0.3,delay:0.2}}
-      
-      className="capitalize text-sm whitespace-nowrap">{name}</motion.span>}
-
+        {isOpen && (
+          <motion.span
+            initial={{ opacity: 0, width: 0 }}
+            animate={{ opacity: 1, width: "auto" }}
+            exit={{ opacity: 0, width: 0 }}
+            transition={{ duration: 0.3, delay: 0.2 }}
+            className="capitalize text-sm whitespace-nowrap"
+          >
+            {name}
+          </motion.span>
+        )}
       </AnimatePresence>
     </NavLink>
   );
